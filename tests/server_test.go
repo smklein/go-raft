6d10c3fe838fff1a0f4b74125c1537eb68fbab33
Sean Klein <smklein@andrew.cmu.edu>
2015-04-09 16:21:20 -0400

    Added server management package for testing

--- tests/server_test.go
+++ tests/server_test.go
@@ -2,12 +2,26 @@ package tests
 
 import (
 	"server"
+	"serverManagement"
 	"testing"
 )
 
 func TestBasic(t *testing.T) {
-	t.Logf("Test basic started")
 	if !server.GimmeTrue() {
+		t.Errorf("Failed")
+	}
+}
+
+func TestServerStartup(t *testing.T) {
+	t.Logf("Test Server Startup")
+	sm := serverManagement.StartAllServers()
+	t.Logf("All servers started")
+	if err := sm.KillServer("server1"); err != nil {
+		t.Log(err)
+		t.Errorf("Failed")
+	}
+	if err := sm.RestartServer("server1"); err != nil {
+		t.Log(err)
 		t.Errorf("Failed")
 	}
 }
