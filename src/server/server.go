9052d0fe217bc16050fd5b6d74876a98fe92f492
Sean Klein <smklein@andrew.cmu.edu>
2015-04-09 15:34:19 -0400

    Added normal server runner

--- src/server/server.go
+++ src/server/server.go
@@ -1,35 +1,26 @@
 package server
 
 import (
+	"config"
 	"fmt"
-	"raftRpc"
+	//"raftRpc"
 )
 
-// TODO Get rid of these...
-var (
-	serverAddr1 = "localhost:8081"
-	serverAddr2 = "localhost:9002"
-	serverAddr3 = "localhost:9003"
-	serverAddr4 = "localhost:9004"
-)
+type RaftServer int
 
-func main() {
-	fmt.Println("Hi from main!")
-	rc, err := raftRpc.DialHTTP("tcp", serverAddr1)
-	if err != nil {
-		fmt.Println("Cannot dial: " + serverAddr1)
-		return
-	}
-	fmt.Println("Successfully dialed: ", serverAddr1)
-	args := "this is the args"
-	var reply string
-	err = rc.Call("Check.CheckRPC", args, &reply)
-	if err != nil {
-		fmt.Println("Whoops! We can't dial checkrpc.")
-		return
-	}
-	fmt.Println("Main sees reply from server: " + reply)
-	rc.Close()
+var Cf config.Config
+
+type RaftServerInput struct {
+	placeholder string
+}
+
+type RaftServerOutput struct {
+	placeholder string
+}
+
+func (rs *RaftServer) Commit(in RaftServerInput, out *RaftServerOutput) error {
+	fmt.Println("[RAFT SERVER] Commit Called")
+	return nil
 }
 
 func GimmeTrue() bool {
