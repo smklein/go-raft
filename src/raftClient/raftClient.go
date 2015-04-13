e32a2f2b947b7bd1dbe460263611acf87ae2a418
Advaya Krishna <advayakrishna@gmail.com>
2015-04-13 18:42:18 -0400

    Added DebugGetRaftLeader to raft client

--- src/raftClient/raftClient.go
+++ src/raftClient/raftClient.go
@@ -2,10 +2,12 @@ package raftClient
 
 import (
 //"net/rpc"
+	"errors"
 )
 
 type RaftClient struct {
 	test int
+	serverNames []string
 }
 
 func (client *RaftClient) Commit(value string) error {
@@ -29,4 +31,26 @@ DEBUG function, asks if server is "leader", "follower", or "candidate"
 */
 func (client *RaftClient) DebugGetServerStatus(server string) (string, error) {
 	return "", nil
+}
+
+func (client *RaftClient) DebugGetRaftLeader() (string, error) {
+	var leader string
+	for _, server := range(client.serverNames) {
+		status, err := client.DebugGetServerStatus(server)
+		if err != nil {
+			return "", err
+		}
+		if leader == "" {
+			if status == "leader" {
+				leader = server
+			}
+		} else if status == "leader" {
+			return "", errors.New("Multiple leaders detected")
+		}
+	}
+	if leader == "" {
+		return "", errors.New("No leader found")
+	} else {
+		return leader, nil
+	}
 }
